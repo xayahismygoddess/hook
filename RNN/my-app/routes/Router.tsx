9f36a29464c732921a5949c1b0553d887f43344c
Xayahismygoddess <balderramamax@gmail.com>
2024-06-24 17:44:18 -0600

    delete

--- RNN/my-app/routes/Router.tsx
+++
@@ -1,8 +0,0 @@
-import { View,Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import AuthStack from "./AuthStack";
-import AppStack from "./AppStack";
-
-
-
-function
